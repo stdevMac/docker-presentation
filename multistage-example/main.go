1f29d32c60b30b7c82ff41c2fc1c95d8df943779
Marcos Maceo <marcosmaceowork@gmail.com>
2019-11-22 22:07:18 -0500

    Add multistage builds on dockerfiles

---
+++ multistage-example/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	serverAddress = ":3030"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("welcome");
+	fmt.Fprintf(w, "Hello welcome %s!", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	log.Println("Listening on port ", serverAddress)
+	log.Fatal(http.ListenAndServe(serverAddress, nil))
+}
